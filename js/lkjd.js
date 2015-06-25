ddebb47d811c2f73b22820823e2a6689c6cc60f4
Trrapp12 <trrapp@gmail.com>
2015-06-25 13:17:01 -0600

    fixed broken links, changed button colors, added navigation functionality between bio cards

---
+++ js/lkjd.js
@@ -0,0 +1,11 @@
+/**
+ * Created by trevor on 6/25/15.
+ */
+server {
+    listen 80;
+
+    root /var/www/web-karma.org/Web-Karma;
+    index index.html index.htm;
+
+    server_name web-karma.org;
+}
